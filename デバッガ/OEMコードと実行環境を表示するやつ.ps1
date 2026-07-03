d85392f4e495a806ec37361cd36a7e6cb72765b9
fuaikeFu <furdev.2ndgen@gmail.com>
2026-07-04 03:47:32 +0900

    issue#1について検証

--- デバッガ/OEMコードと実行環境を表示するやつ.ps1
+++ デバッガ/OEMコードと実行環境を表示するやつ.ps1
@@ -1,6 +1,6 @@
-        $oemCodePage = [System.Globalization.CultureInfo]::CurrentCulture.TextInfo.OEMCodePage
-        Write-Host "($oemCodePage)"
-        $psVer  = "PowerShell $($PSVersionTable.PSVersion.ToString())"
-        Write-Host "($psVer)"
+        $oemCodeP = [System.Globalization.CultureInfo]::CurrentCulture.TextInfo.OEMCodePage
+        Write-Host "($oemCodeP)"
+        $psV  = "PowerShell $($PSVersionTable.PSVersion.ToString())"
+        Write-Host "($psV)"
 Write-Host "Press any key to continue..." -NoNewline
 $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
